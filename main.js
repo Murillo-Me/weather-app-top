a5be22e07abee47f54d9d8312ee177c7a3c64dbc
Murillo Mendes <murillomendes95@hotmail.com>
2021-12-04 23:18:00 -0300

    Added functionality to change cities and timezone

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frid
 const monthOfYear = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
 
 let city = 'Curitiba'
+let timezone = -10800
 
 function runClock() {
-    const date = new Date();
+    const date = getTimezoneDate()
     const d = {
                 day: dayOfWeek[date.getDay()],
                 month: monthOfYear[date.getMonth()],
@@ -19,13 +20,22 @@ function runClock() {
     dateText.innerText = `${d.day}, ${d.month} ${d.date}th, ${d.year}`
 
     const dateClock = document.querySelector('.current-time')
-    dateClock.innerText = checkTime(date.getHours()) + ':' + checkTime(date.getMinutes())
+    dateClock.innerText = checkTime(date.getUTCHours()) + ':' + checkTime(date.getUTCMinutes())
 }
 
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
   }
+
+function getTimezoneDate() {
+    var date = new Date(); 
+    var now_utc =  Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
+    date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
+    now_utc = now_utc + timezone*1000
+
+    return new Date(now_utc);
+}
 
 function fahrenheitToCelsius(fahrenheitTemp) {
     return celsiusTemp = (fahrenheitTemp - 32) * 5/9
@@ -51,6 +61,26 @@ async function getForecastWeatherData(cityName) {
 
 runClock()
 
+const cityElement = document.querySelector('.city-title')
+cityElement.addEventListener('click', () => {
+    const inputElement = document.createElement('input')
+    cityElement.parentElement.append(inputElement)
+    cityElement.style.display = 'none'
+
+    inputElement.focus()
+    inputElement.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            city = inputElement.value
+            cityElement.style.display = 'flex'
+            cityElement.innerHTML = city + " <i class='fas fa-edit fa-xs'></i>"
+            inputElement.remove()
+            getData()
+        }
+    })
+})
+
+function getData() {
+
 getCurrentWeatherData(city).then(data => {
     let tempElement = document.querySelector('.current-temp')
     let minTempElement = document.querySelector('.min-temp')
@@ -61,6 +91,7 @@ getCurrentWeatherData(city).then(data => {
     currentWeatherData.minTemp = kelvinToCelsius(data.main.temp_min).toFixed(1)
     currentWeatherData.maxTemp = kelvinToCelsius(data.main.temp_max).toFixed(1)
     currentWeatherData.humidity = data.main.humidity
+    timezone = data.timezone
 
     tempElement.innerHTML = "<i class='fas fa-thermometer-half'></i>" + currentWeatherData.temp + 'ºC'
     minTempElement.innerText = currentWeatherData.minTemp + 'ºC'
@@ -72,4 +103,7 @@ getForecastWeatherData(city).then(data => {
     console.log(data)
 })
 
+}
+
 setInterval(runClock, 1000);
+getData()
